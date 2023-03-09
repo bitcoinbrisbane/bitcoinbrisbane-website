2abdf0f3c1df8904e58bf1be49b5bc77b3c4f238
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-09 10:54:54 +1000

    Initial commit

---
+++ src/Routing.tsx
@@ -0,0 +1,21 @@
+import { lazy } from "react";
+import { AppRoutes } from "./types/app";
+import PageOne from "./pages/PageOne";
+
+const PageTwo = lazy(() => import("./pages/PageTwo"));
+
+const Routing: AppRoutes = [
+  {
+    path: "/",
+    element: <PageOne />
+  },
+  {
+    path: "/page-two",
+    element: <PageTwo />
+  }
+];
+
+export const RoutingWithoutElements = Routing.map(r => r.path);
+
+export { default as NotFound } from "./pages/NotFound";
+export default Routing;
