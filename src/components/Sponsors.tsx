8d4e4375290570bcd848067ac6d8aab89b0cf309
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-03-10 15:59:47 +1000

    chore: DLTx Labs

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -10,6 +10,19 @@ export const Sponsors: React.FC = () => (
       <div className="w-fit pb-12 lg:pb-32">
         <div className="flex items-center gap-x-8">
           <img
+            src={DLTx}
+            alt="DLTx Labs logo"
+            className="hidden lg:block w-[10vw] scale-[0.5] relative bottom-8"
+          />
+          <p className="mb-6 lg:mb-12">
+            <span className="text-3xl lg:text-5xl">DLTX LABS</span>
+            <br />
+            Brisbane&apos;s best blockchain venture studio for their support and
+            Ledger Hardware Wallet door prizes!
+          </p>
+        </div>
+        <div className="flex items-center gap-x-8">
+          <img
             src={SwyftX}
             alt="SwyftX logo"
             className="hidden lg:block w-[10vw] relative bottom-8"
@@ -22,19 +35,7 @@ export const Sponsors: React.FC = () => (
             events.
           </p>
         </div>
-        <div className="flex items-center gap-x-8">
-          <img
-            src={DLTx}
-            alt="DLTx Labs logo"
-            className="hidden lg:block w-[10vw] scale-[0.5] relative bottom-8"
-          />
-          <p className="mb-6 lg:mb-12 ">
-            <span className="text-3xl lg:text-5xl">DLTX</span>
-            <br />
-            Brisbane&apos;s best blockchain venture studio for their support and
-            Ledger Hardware Wallet door prizes!
-          </p>
-        </div>
+
         <div className="flex items-center gap-x-8">
           <img
             src={Mycelium}
