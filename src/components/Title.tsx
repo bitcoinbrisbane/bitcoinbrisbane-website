ccf4a4fe9c7d5eb4963d2712cfa9fc35acf9fd18
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-03-13 10:02:40 +1000

    feat: add new tags

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "./Button";
 
 export const Title: React.FC = () => (
   <div className="flex flex-col items-center text-white z-50 relative pt-16 lg:px-24">
@@ -8,9 +9,22 @@ export const Title: React.FC = () => (
       BRISBANE
     </h1>
     <h2 className="font-semibold subtitle text-lg lg:text-4xl w-full text-center lg:text-left leading-[1.2rem] mt-4">
-      BRISBANE&apos;S BLOCKCHAIN AND
+      BRISBANE&apos;S OLDEST BLOCKCHAIN AND
       <br />
-      BITCOIN OLDEST COMMUNITY GROUP
+      BITCOIN COMMUNITY GROUP
     </h2>
+    <div className="mt-4 lg:mt-10 lg:pl-[5%] lg:w-full">
+      <Button
+        title="BOOK A CONSULTATION WITH DLTX"
+        onClick={() =>
+          // robots can still crawl javascript links so we don't lose SEO
+          window.open(
+            "https://outlook.office365.com/owa/calendar/BitcoinBrisbane@bitcoinbrisbane.com.au/bookings/",
+            "_blank",
+            "noopener noreferrer"
+          )
+        }
+      />
+    </div>
   </div>
 );
