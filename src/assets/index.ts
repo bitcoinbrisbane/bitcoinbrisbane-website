c20fb18ab0c826ae39285a9833d2811fc9815029
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-05-03 11:51:17 +1000

    chore: roll out date

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,6 @@
-export { default as BlankDate } from "./BlankDate.png";
+export { default as Date } from "./BlankDate.png";
 export { default as Cloud } from "./Cloud.png";
 export { default as Coin } from "./Coin.png";
-export { default as Date } from "./Date.png";
 export { default as Hill } from "./Hill.png";
 
 export { default as BA } from "./BA.png";
