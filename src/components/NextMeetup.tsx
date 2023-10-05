af02c2a7cc73f03e3b08243a4814490dc46903ae
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-10-05 13:05:52 +1000

    chore: add token

--- src/components/NextMeetup.tsx
+++ src/components/NextMeetup.tsx
@@ -32,21 +32,28 @@ export const NextMeetup = () => {
 
   const convertDate = (date: string) => {
     const splitDate = date.split("-");
-    const finalDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
+    if (splitDate.length !== 3) return date;
 
+    const finalDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
     return finalDate;
   };
 
   const convertTime = (time: string) => {
     const splitTime = time.split(":");
-    const finalTime = `${splitTime[0]}:${splitTime[1]}`;
+    if (splitTime.length !== 2) return time;
 
+    const finalTime = `${splitTime[0]}:${splitTime[1]}`;
     return finalTime;
   };
 
   useEffect(() => {
     axios
-      .get<MeetupEvent>("http://cms.dltx.io/api/events?populate=*")
+      // Add bearer token to get past CORS
+      .get<MeetupEvent>("http://cms.dltx.io/api/events?populate=*", {
+        headers: {
+          Authorization: `Bearer 077fa26ea22e6b25c78279efd6d7d6fca8194fb949ebcbeb0ce3fc4612a13883675a420d78479cc282dc04cf17a2bf5895d8b15fc77dd132c018845d3b20ebb3d2cfc8dc63cbfc25ba1fcfd574bb5178187dd29cf49014fd3a385efc431030b2455106ec2287d8324f125e3ef98da20a97f9e5c28e007a0d9e3a381d3c4e9e6a`
+        }
+      })
       .then(({ data }) => {
         setEvents(data.data);
       })
