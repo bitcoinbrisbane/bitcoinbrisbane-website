0af559a76b1515f6d87a47e0fcfc1513e15f5d73
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-01-22 18:23:19 +1000

    feat: fixing build

--- src/components/PreviousMeetups.tsx
+++ src/components/PreviousMeetups.tsx
@@ -1,36 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { MeetupEvent } from "../types/app";
+import { MeetupEvent, MeetupResponse } from "../types/app";
 
 export const PreviousMeetup = () => {
-  const [events, setEvents] = useState<any[]>();
-
-  const convertDate = (date: string) => {
-    const splitDate = date.split("-");
-    if (splitDate.length !== 3) return date;
-
-    const finalDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
-    return finalDate;
-  };
-
-  const convertTime = (time: string) => {
-    const splitTime = time.split(":");
-    if (splitTime.length !== 2) return time;
-
-    const finalTime = `${splitTime[0]}:${splitTime[1]}`;
-    return finalTime;
-  };
+  const [event, setEvent] = useState<MeetupEvent>();
 
   useEffect(() => {
     axios
       // Add bearer token to get past CORS
-      .get<MeetupEvent>("https://cms.dltx.io/api/events?populate=*", {
+      .get<MeetupResponse>("https://cms.dltx.io/api/events?populate=*", {
         headers: {
           Authorization: `Bearer 077fa26ea22e6b25c78279efd6d7d6fca8194fb949ebcbeb0ce3fc4612a13883675a420d78479cc282dc04cf17a2bf5895d8b15fc77dd132c018845d3b20ebb3d2cfc8dc63cbfc25ba1fcfd574bb5178187dd29cf49014fd3a385efc431030b2455106ec2287d8324f125e3ef98da20a97f9e5c28e007a0d9e3a381d3c4e9e6a`
         }
       })
       .then(({ data }) => {
-        setEvents(data.data);
+        setEvent(data.data[0]);
       })
       .catch((error: any) => {
         console.log(error);
@@ -43,7 +27,7 @@ export const PreviousMeetup = () => {
         PREVIOUS MEETUPS
       </h2>
       <div className="w-full mt-10 lg:mt-24 font-hand lg:flex">
-        
+        <p>{event?.id}</p>
       </div>
     </section>
   );
