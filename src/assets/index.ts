8ed0b60dd9474567870d965a7b0a9fad66885c54
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-01-26 09:20:28 +1000

    feat: add pizzdao

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,3 +6,4 @@ export { default as Hill } from "./Hill.png";
 export { default as BA } from "./BA.png";
 export { default as DLTx } from "./DLTx.png";
 export { default as Bridge } from "./Bridge.png";
+export { default as Pizza } from "./pizzaguy.png";
