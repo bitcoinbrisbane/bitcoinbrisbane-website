03586b59d3bbeff06152a0bd35dd76a29bb98bb0
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-01-29 09:21:26 +1000

    feat: get blocks

--- src/components/Halvening.tsx
+++ src/components/Halvening.tsx
@@ -1,42 +1,41 @@
-import { Pizza } from "../assets";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-export const Halvening = async () => {
+export const Halvening = () => {
+  const [height, setHeight] = useState("Loading blocks...");
 
   const getHeight = async () => {
-    const body = {
-      jsonrpc: "1.0",
-      method: "getBlckHeight",
-      id: "1",
-      parameter: [],
-    };
-    const port = 8332;
     try {
-      const response = await axios.post(`http://explorer.getpaidinbitcoin.com.au:8332`, body, {
-        auth: {
-          username: "test",
-          password: "test",
-        },
-      });
-      console.log(response.data);
+      const response = await axios.get(
+        "https://api2.getpaidinbitcoin.com.au/nodes"
+      );
+
+      console.log(response);
+      return "Height " + response.data[0].height.toLocaleString();
     } catch (error) {
-      // console.log(error.response.data);
+      //
+      return "Error getting blocks";
     }
   };
 
-  const blocks = await getHeight();
+  useEffect(() => {
+    getHeight().then(height => {
+
+      setHeight(height);
+    });
+  }, []);
 
   return (
     <section className="w-full mt-12 px-6">
       <h2 className="font-black title text-5xl lg:text-9xl w-full text-center text-white">
-        HALVENING 2024! {blocks}
+        HALVENING 2024!
       </h2>
 
       <div className="w-4/5 mx-auto font-hand">
         <div className="flex w-full">
           <div className="flex-shrink-0 max-w-[60%] p-4">
             <h3 className="text-3xl lg:text-6xl leading-[1.5rem] relative top-1">
-              todo
+              {height}
             </h3>
             <p className="mt-4 text-2xl">
               The amount of bitcoins rewarded for each block decreases with
@@ -47,14 +46,14 @@ export const Halvening = async () => {
               is a popular meme among bitcoiners.
             </p>
             <p className="mt-4 text-2xl">lipsum</p>
-            <span className="block md:text-4xl text-red-700">
+            {/* <span className="block md:text-4xl text-red-700">
               RSVP AT meetup.com/bitcoinbrisbane
-            </span>
+            </span> */}
           </div>
           <div className="flex-grow p-4">
-            <img src={Pizza} alt="Bitcoin Pizza DAO"></img>
+            add image
             <p className="mt-4 text-2xl">
-              Thanks to https://www.thehalvening.com
+              Full node sponsored by Get Paid In Bitcoin
             </p>
           </div>
         </div>
