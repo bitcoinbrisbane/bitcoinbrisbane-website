d9511eadc22e6ee89b66ac2be7c5f0ce32ab25e1
rodo <rodonguyendd@gmail.com>
2024-04-22 13:38:18 +1000

    chore: remove unused assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,7 @@
-export { default as Date } from "./BlankDate.png";
 export { default as Cloud } from "./Cloud.png";
 export { default as Coin } from "./Coin.png";
 export { default as Hill } from "./Hill.png";
-
-export { default as BA } from "./BA.png";
-export { default as DLTx } from "./DLTx.png";
 export { default as Bridge } from "./Bridge.png";
+
 export { default as Pizza } from "./pizzaguy.png";
 export { default as Rocket } from "./fusee-rampe.png";
