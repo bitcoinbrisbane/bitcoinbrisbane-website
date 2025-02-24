7d9a6839187e85cb064e0ae8c71f3920411849bd
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-02-25 08:18:21 +1000

    fix: Update React imports and improve iframe attributes for consistency; upgrade dependencies

--- src/components/Halvening.tsx
+++ src/components/Halvening.tsx
@@ -43,10 +43,10 @@ export const Halvening = () => {
             height="630"
             src="https://www.youtube.com/embed/5Tu3fsSunSY?si=IX4Un_bQVuh_dpeC"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
 
