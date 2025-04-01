967bab9933ef6aeae2711637d8b48599139a76e1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-04-02 09:12:09 +1000

    chore: remove imports

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,20 +1,452 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Sponsor, SponsorResponse } from "../types/app";
+const json = {
+  data: [
+    {
+      id: 3,
+      attributes: {
+        name: "SWYFTX",
+        description:
+          "Australia's favourite exchange SwyftX for their continual support of the Brisbane bitcoin and blockchain communitiy and meetup events.",
+        createdAt: "2023-11-16T01:29:25.748Z",
+        updatedAt: "2023-12-21T22:50:21.642Z",
+        publishedAt: "2023-11-16T01:29:30.597Z",
+        logo: {
+          data: {
+            id: 79,
+            attributes: {
+              name: "SwyftX.png",
+              alternativeText: null,
+              caption: null,
+              width: 6586,
+              height: 1908,
+              formats: {
+                large: {
+                  ext: ".png",
+                  url: "https://bafkreidehktmcbc2lbsf6jicy3lusb6nyvyl7tfc7ackkvmnxvnyfnxtzm.ipfs.dweb.link",
+                  hash: "large_Swyft_X_24339facf4",
+                  mime: "image/png",
+                  name: "large_SwyftX.png",
+                  path: null,
+                  size: 37.63,
+                  width: 1000,
+                  height: 290
+                },
+                small: {
+                  ext: ".png",
+                  url: "https://bafkreih3qx5i2enfbhc6p5jlfjpmmu64v4q4np6rtsqkmayxbolnzcpzty.ipfs.dweb.link",
+                  hash: "small_Swyft_X_24339facf4",
+                  mime: "image/png",
+                  name: "small_SwyftX.png",
+                  path: null,
+                  size: 17.1,
+                  width: 500,
+                  height: 145
+                },
+                medium: {
+                  ext: ".png",
+                  url: "https://bafkreibv5cgiaqjks64bbmgwfpunlodcjfjofcgf5xyfinnuwrok555q3y.ipfs.dweb.link",
+                  hash: "medium_Swyft_X_24339facf4",
+                  mime: "image/png",
+                  name: "medium_SwyftX.png",
+                  path: null,
+                  size: 27.14,
+                  width: 750,
+                  height: 217
+                },
+                thumbnail: {
+                  ext: ".png",
+                  url: "https://bafkreidtdhrzpx7elraerzakuzhau7tndl5xppmdh4eglnn23h7mawvtzq.ipfs.dweb.link",
+                  hash: "thumbnail_Swyft_X_24339facf4",
+                  mime: "image/png",
+                  name: "thumbnail_SwyftX.png",
+                  path: null,
+                  size: 8,
+                  width: 245,
+                  height: 71
+                }
+              },
+              hash: "Swyft_X_24339facf4",
+              ext: ".png",
+              mime: "image/png",
+              size: 69.38,
+              url: "https://bafkreid37catqdgi6vo2pkbg4eojcrft7bla4p7673b4ibmqddisr7o4dm.ipfs.dweb.link",
+              previewUrl: null,
+              provider: "strapi-provider-upload-ipfs-storage",
+              provider_metadata: null,
+              createdAt: "2023-12-21T22:49:20.368Z",
+              updatedAt: "2023-12-21T22:49:20.368Z"
+            }
+          }
+        }
+      }
+    },
+    {
+      id: 4,
+      attributes: {
+        name: "BLOCKCHAIN AUSTRALIA",
+        description:
+          "For their continual support of the Brisbane bitcoin and blockchain community and meetup events.",
+        createdAt: "2023-11-16T01:30:03.994Z",
+        updatedAt: "2024-01-24T03:43:52.185Z",
+        publishedAt: "2023-11-16T01:30:08.850Z",
+        logo: {
+          data: {
+            id: 80,
+            attributes: {
+              name: "Blockchain-Australia.png",
+              alternativeText: null,
+              caption: null,
+              width: 1878,
+              height: 674,
+              formats: {
+                large: {
+                  ext: ".png",
+                  url: "https://bafkreicvfxdkcns3fvf4iurlju4m6hofbhspwpcabxc5bonzsv2rkj56fm.ipfs.dweb.link",
+                  hash: "large_Blockchain_Australia_c83a3763d7",
+                  mime: "image/png",
+                  name: "large_Blockchain-Australia.png",
+                  path: null,
+                  size: 94.29,
+                  width: 1000,
+                  height: 359
+                },
+                small: {
+                  ext: ".png",
+                  url: "https://bafkreidqyvsikelhp66vkc3j6mxoqrjdfugiopjr4ht7xqrz6qzd673cxm.ipfs.dweb.link",
+                  hash: "small_Blockchain_Australia_c83a3763d7",
+                  mime: "image/png",
+                  name: "small_Blockchain-Australia.png",
+                  path: null,
+                  size: 30.27,
+                  width: 500,
+                  height: 179
+                },
+                medium: {
+                  ext: ".png",
+                  url: "https://bafkreiealpncnu5v5eqcmjzgnovkxmi6t2xn2omdogywlsapuknj53mfsm.ipfs.dweb.link",
+                  hash: "medium_Blockchain_Australia_c83a3763d7",
+                  mime: "image/png",
+                  name: "medium_Blockchain-Australia.png",
+                  path: null,
+                  size: 54.35,
+                  width: 750,
+                  height: 269
+                },
+                thumbnail: {
+                  ext: ".png",
+                  url: "https://bafkreibp5ualfbpgmowztm3lsgvuyd7q5vb7lxycefjattp2g3rrmhjq3y.ipfs.dweb.link",
+                  hash: "thumbnail_Blockchain_Australia_c83a3763d7",
+                  mime: "image/png",
+                  name: "thumbnail_Blockchain-Australia.png",
+                  path: null,
+                  size: 12.68,
+                  width: 245,
+                  height: 88
+                }
+              },
+              hash: "Blockchain_Australia_c83a3763d7",
+              ext: ".png",
+              mime: "image/png",
+              size: 52.19,
+              url: "https://bafkreigapcd4aul3gdzp6p7wttabpleizd4adh7pdgsagu7r73xr4gxony.ipfs.dweb.link",
+              previewUrl: null,
+              provider: "strapi-provider-upload-ipfs-storage",
+              provider_metadata: null,
+              createdAt: "2024-01-24T01:33:07.278Z",
+              updatedAt: "2024-01-24T01:33:07.278Z"
+            }
+          }
+        }
+      }
+    },
+    {
+      id: 5,
+      attributes: {
+        name: "HASHLOCK",
+        description:
+          "Australia's leading blockchain security and smart contract auditing firm. Diligent smart contract audits and blockchain security services.",
+        createdAt: "2023-11-16T01:37:24.100Z",
+        updatedAt: "2024-01-24T03:54:38.652Z",
+        publishedAt: "2023-11-16T01:37:29.705Z",
+        logo: {
+          data: {
+            id: 81,
+            attributes: {
+              name: "Hashlock-Emblem-transparent.png",
+              alternativeText: null,
+              caption: null,
+              width: 5000,
+              height: 5000,
+              formats: {
+                large: {
+                  ext: ".png",
+                  url: "https://bafkreihjhuzrxvkoihpqxihp6alklmuuh5yawekw7273glug2jern2bi3e.ipfs.dweb.link",
+                  hash: "large_Hashlock_Emblem_transparent_261d0db0df",
+                  mime: "image/png",
+                  name: "large_Hashlock-Emblem-transparent.png",
+                  path: null,
+                  size: 86.06,
+                  width: 1000,
+                  height: 1000
+                },
+                small: {
+                  ext: ".png",
+                  url: "https://bafkreibcilf7nybpjs3gc5znnslsjrprpv4hhdcq6hyy6koet6e5fcrk5e.ipfs.dweb.link",
+                  hash: "small_Hashlock_Emblem_transparent_261d0db0df",
+                  mime: "image/png",
+                  name: "small_Hashlock-Emblem-transparent.png",
+                  path: null,
+                  size: 33.39,
+                  width: 500,
+                  height: 500
+                },
+                medium: {
+                  ext: ".png",
+                  url: "https://bafkreie3hj7cwmywxo6mmtyxhgav5cla2gmzhcbf3cgxu4ukmof2anqgtq.ipfs.dweb.link",
+                  hash: "medium_Hashlock_Emblem_transparent_261d0db0df",
+                  mime: "image/png",
+                  name: "medium_Hashlock-Emblem-transparent.png",
+                  path: null,
+                  size: 61.72,
+                  width: 750,
+                  height: 750
+                },
+                thumbnail: {
+                  ext: ".png",
+                  url: "https://bafkreiecl7z5rgxlnwt3gph3hpmxqmkigxtxotbm3sanrooqytke2s5kze.ipfs.dweb.link",
+                  hash: "thumbnail_Hashlock_Emblem_transparent_261d0db0df",
+                  mime: "image/png",
+                  name: "thumbnail_Hashlock-Emblem-transparent.png",
+                  path: null,
+                  size: 9.79,
+                  width: 156,
+                  height: 156
+                }
+              },
+              hash: "Hashlock_Emblem_transparent_261d0db0df",
+              ext: ".png",
+              mime: "image/png",
+              size: 81.22,
+              url: "https://bafkreifucwgfesy6pihefmrm3qyjbrgcos2s75gjglqn6b574haeev3c7m.ipfs.dweb.link",
+              previewUrl: null,
+              provider: "strapi-provider-upload-ipfs-storage",
+              provider_metadata: null,
+              createdAt: "2024-01-24T03:52:23.636Z",
+              updatedAt: "2024-01-24T03:52:23.636Z"
+            }
+          }
+        }
+      }
+    },
+    {
+      id: 2,
+      attributes: {
+        name: "GET PAID IN BITCOIN",
+        description: "The easiest way to stack stats.",
+        createdAt: "2023-11-16T01:28:49.454Z",
+        updatedAt: "2024-01-24T05:46:47.530Z",
+        publishedAt: "2023-11-16T01:28:54.106Z",
+        logo: {
+          data: {
+            id: 88,
+            attributes: {
+              name: "GetPaidInBitcoin-blue.png",
+              alternativeText: null,
+              caption: null,
+              width: 808,
+              height: 572,
+              formats: {
+                small: {
+                  ext: ".png",
+                  url: "https://bafkreieci3ukxmongbksdgykhtznq36uhqpp2vthc3j76jumupvxoipj3u.ipfs.dweb.link",
+                  hash: "small_Get_Paid_In_Bitcoin_blue_191585d657",
+                  mime: "image/png",
+                  name: "small_GetPaidInBitcoin-blue.png",
+                  path: null,
+                  size: 49.39,
+                  width: 500,
+                  height: 354
+                },
+                medium: {
+                  ext: ".png",
+                  url: "https://bafkreialevqgresgycfpu2efem6fp6dbfhluiun5bg6wevugg65oqtslna.ipfs.dweb.link",
+                  hash: "medium_Get_Paid_In_Bitcoin_blue_191585d657",
+                  mime: "image/png",
+                  name: "medium_GetPaidInBitcoin-blue.png",
+                  path: null,
+                  size: 79.76,
+                  width: 750,
+                  height: 531
+                },
+                thumbnail: {
+                  ext: ".png",
+                  url: "https://bafkreieyywkblg2ayaj45jkbsmzdwn3s35zfa3xoo3zafrfia5sglzre5u.ipfs.dweb.link",
+                  hash: "thumbnail_Get_Paid_In_Bitcoin_blue_191585d657",
+                  mime: "image/png",
+                  name: "thumbnail_GetPaidInBitcoin-blue.png",
+                  path: null,
+                  size: 19.36,
+                  width: 220,
+                  height: 156
+                }
+              },
+              hash: "Get_Paid_In_Bitcoin_blue_191585d657",
+              ext: ".png",
+              mime: "image/png",
+              size: 11.62,
+              url: "https://bafkreiedolb4phyhmyk6ctfzp6w4rabgjpktctjpvxy5ar55kyadcvvomq.ipfs.dweb.link",
+              previewUrl: null,
+              provider: "strapi-provider-upload-ipfs-storage",
+              provider_metadata: null,
+              createdAt: "2024-01-24T05:46:08.832Z",
+              updatedAt: "2024-01-24T05:46:08.832Z"
+            }
+          }
+        }
+      }
+    },
+    {
+      id: 1,
+      attributes: {
+        name: "DLTx LABS",
+        description:
+          "Brisbane's best blockchain venture studio for their support and Ledger Hardware Wallet door prizes!",
+        createdAt: "2023-11-16T01:28:14.047Z",
+        updatedAt: "2024-04-22T13:05:43.536Z",
+        publishedAt: "2023-11-16T01:28:18.890Z",
+        logo: {
+          data: {
+            id: 115,
+            attributes: {
+              name: "DLTX Labs - Single Colour Black.png",
+              alternativeText: null,
+              caption: null,
+              width: 3253,
+              height: 500,
+              formats: {
+                large: {
+                  ext: ".png",
+                  url: "https://bafkreieffmrod5alhjiu6sfmv3klrihcw2w5rn435uhegcqo56tu7fr7xq.ipfs.dweb.link",
+                  hash: "large_DLTX_Labs_Single_Colour_Black_76798e5726",
+                  mime: "image/png",
+                  name: "large_DLTX Labs - Single Colour Black.png",
+                  path: null,
+                  size: 18.14,
+                  width: 1000,
+                  height: 154
+                },
+                small: {
+                  ext: ".png",
+                  url: "https://bafkreicyotoangk6cbhywmgzwj37j7aidc2wi55fi3xa32kxoeug63ochi.ipfs.dweb.link",
+                  hash: "small_DLTX_Labs_Single_Colour_Black_76798e5726",
+                  mime: "image/png",
+                  name: "small_DLTX Labs - Single Colour Black.png",
+                  path: null,
+                  size: 8.68,
+                  width: 500,
+                  height: 77
+                },
+                medium: {
+                  ext: ".png",
+                  url: "https://bafkreievrb3mpw3ugibp7y42phs27zrjg2dc5ut3wiuqgiad2pu5yvmkky.ipfs.dweb.link",
+                  hash: "medium_DLTX_Labs_Single_Colour_Black_76798e5726",
+                  mime: "image/png",
+                  name: "medium_DLTX Labs - Single Colour Black.png",
+                  path: null,
+                  size: 13.6,
+                  width: 750,
+                  height: 115
+                },
+                thumbnail: {
+                  ext: ".png",
+                  url: "https://bafkreicngvzhcksoqabgsspddnhwd4h4szczloubreyz3biq7bd3t2nwwm.ipfs.dweb.link",
+                  hash: "thumbnail_DLTX_Labs_Single_Colour_Black_76798e5726",
+                  mime: "image/png",
+                  name: "thumbnail_DLTX Labs - Single Colour Black.png",
+                  path: null,
+                  size: 4.82,
+                  width: 245,
+                  height: 38
+                }
+              },
+              hash: "DLTX_Labs_Single_Colour_Black_76798e5726",
+              ext: ".png",
+              mime: "image/png",
+              size: 19.41,
+              url: "https://bafkreidracbmikzakl45dvm3oxo6an5piyeemmvtjvvhc6xefm7u4xzdvq.ipfs.dweb.link",
+              previewUrl: null,
+              provider: "strapi-provider-upload-ipfs-storage",
+              provider_metadata: null,
+              createdAt: "2024-04-22T12:59:48.929Z",
+              updatedAt: "2024-04-22T12:59:48.929Z"
+            }
+          }
+        }
+      }
+    },
+    {
+      id: 7,
+      attributes: {
+        name: "Virgo Australia",
+        description: "Australia's Trusted Crypto Exchange",
+        createdAt: "2024-12-15T01:11:03.178Z",
+        updatedAt: "2024-12-15T01:21:58.933Z",
+        publishedAt: "2024-12-15T01:21:58.739Z",
+        logo: {
+          data: {
+            id: 148,
+            attributes: {
+              name: "virgo.png",
+              alternativeText: null,
+              caption: null,
+              width: 316,
+              height: 93,
+              formats: {
+                thumbnail: {
+                  ext: ".png",
+                  url: "https://bafkreicddfollwjfmiaftmvy54sqht5j243mf3a5dbka4g7xpjaax3dy4e.ipfs.dweb.link",
+                  hash: "thumbnail_virgo_dc30043587",
+                  mime: "image/png",
+                  name: "thumbnail_virgo.png",
+                  path: null,
+                  size: 12.26,
+                  width: 245,
+                  height: 72
+                }
+              },
+              hash: "virgo_dc30043587",
+              ext: ".png",
+              mime: "image/png",
+              size: 2.6,
+              url: "https://bafkreieplsom2joc7rotmrfwuqtzudg3lrzlwdsfsmsxjfnbgdhz3l5hjm.ipfs.dweb.link",
+              previewUrl: null,
+              provider: "strapi-provider-upload-ipfs-storage",
+              provider_metadata: null,
+              createdAt: "2024-12-15T01:20:23.949Z",
+              updatedAt: "2024-12-15T01:20:23.949Z"
+            }
+          }
+        }
+      }
+    }
+  ],
+  meta: {
+    pagination: {
+      page: 1,
+      pageSize: 25,
+      pageCount: 1,
+      total: 6
+    }
+  }
+};
 
 export const Sponsors = () => {
-  const [sponsors, setSponsors] = useState<Sponsor[]>();
-
-  useEffect(() => {
-    axios
-      .get<SponsorResponse>("https://cms.dltx.io/api/sponsors?populate=*")
-      .then(({ data }) => {
-        setSponsors(data.data);
-      })
-      .catch((error: any) => {
-        console.log(error);
-      });
-  }, []);
+  const sponsors = json.data.map((sponsor: any) => ({
+    attributes: {
+      name: sponsor.attributes.name,
+      description: sponsor.attributes.description,
+      logo: sponsor.attributes.logo
+    }
+  }));
 
   const getLogo = (sponsor: any) => {
     return sponsor.attributes.logo?.data?.attributes?.formats?.thumbnail?.url;
